2d8d4532f71def1ab599204f8c531f39a0861941
rohinivsenthil <rohinivsenthil@gmail.com>
2021-11-14 16:18:51 +0530

    improv: add sidebar styles

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -4,13 +4,27 @@ import * as styles from "./index.module.css";
 import propTypes from "prop-types";
 import { tabs } from "../../constants/sidebar";
 
-const Sidebar = ({ tab, setTab }) => (
-  <div className={styles.sidebar}>
-    {tabs.map(({ name, id, icon }) => (
-      <SidebarItem text={name} selected={tab === id} icon={icon} />
-    ))}
-  </div>
-);
+const Sidebar = ({ tab, setTab }) => {
+  const onClick = (id) => {
+    setTab(id);
+  };
+
+  return (
+    <div className={styles.sidebar}>
+      <div style={{ marginTop: "20px" }}>
+        {tabs.map(({ name, id, icon }) => (
+          <SidebarItem
+            text={name}
+            selected={tab === id}
+            icon={icon}
+            id={id}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 Sidebar.propTypes = {
   tab: propTypes.string.isRequired,
--- client/src/components/SidebarItem/index.jsx
+++ client/src/components/SidebarItem/index.jsx
@@ -4,11 +4,13 @@ import * as styles from "./index.module.css";
 import propTypes from "prop-types";
 import classnames from "classnames";
 
-const SidebarItem = ({ text, selected, icon }) => (
+const SidebarItem = ({ text, selected, icon, id, onClick }) => (
   <div
+    role="button"
     className={classnames(styles.sidebarItem, {
       [styles.sidebarItemSelected]: selected,
     })}
+    onClick={() => onClick(id)}
   >
     <FontAwesomeIcon icon={icon} />
     <div className={styles.sideBarItemText}>{text}</div>
@@ -19,6 +21,8 @@ SidebarItem.propTypes = {
   text: propTypes.string.isRequired,
   selected: propTypes.bool,
   icon: propTypes.any.isRequired,
+  id: propTypes.string.isRequired,
+  onClick: propTypes.func.isRequired,
 };
 
 SidebarItem.defaultProps = {
