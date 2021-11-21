5aa60664336409316fa1f938262b9cefec2d7657
Mukundan314 <mukundan314@gmail.com>
2021-11-21 15:54:14 +0530

    feat: implement Recv in nymclient

--- server/nymclient/nymclient.go
+++ server/nymclient/nymclient.go
@@ -2,6 +2,8 @@ package nymclient
 
 import (
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/gorilla/websocket"
@@ -16,17 +18,17 @@ const (
 	receivedResponseTag    = 0x01
 	selfAddressResponseTag = 0x02
 
-	errCodeEmptyRequest     = 0x01
-	errCodeTooShortRequest  = 0x02
-	errCodeUnknownRequest   = 0x03
-	errCodeMalformedRequest = 0x03
+	ErrCodeEmptyRequest     = 0x01
+	ErrCodeTooShortRequest  = 0x02
+	ErrCodeUnknownRequest   = 0x03
+	ErrCodeMalformedRequest = 0x03
 
-	errCodeEmptyResponse     = 0x80
-	errCodeTooShortResponse  = 0x81
-	errCodeUnknownResponse   = 0x82
-	errCodeMalformedResponse = 0x83
+	ErrCodeEmptyResponse     = 0x80
+	ErrCodeTooShortResponse  = 0x81
+	ErrCodeUnknownResponse   = 0x82
+	ErrCodeMalformedResponse = 0x83
 
-	errCodeOther = 0xff
+	ErrCodeOther = 0xff
 )
 
 type Client struct {
@@ -34,6 +36,86 @@ type Client struct {
 	conn        *websocket.Conn
 	writeMutex  sync.Mutex
 	readMutex   sync.Mutex
+}
+
+type Address []byte
+
+type ErrorResponse struct {
+	Code    byte
+	Message []byte
+}
+
+type ReceivedResponse struct {
+	Message []byte
+	Surb    []byte
+}
+
+type Response struct {
+	Type        string
+	SelfAddress Address
+	Error       *ErrorResponse
+	Received    *ReceivedResponse
+}
+
+func parseErrorResponse(rawResponse []byte) (errCode byte, msg []byte, err error) {
+	if len(rawResponse) < 10 {
+		err = errors.New("Length of error response is too short")
+		return
+	}
+
+	errCode = rawResponse[1]
+	msgLen := int(binary.BigEndian.Uint64(rawResponse[2:10]))
+	msg = rawResponse[10:]
+
+	if len(msg) != msgLen {
+		err = errors.New("Length of message does not match msgLen")
+		return
+	}
+
+	return
+}
+
+func parseReceivedResponse(rawResponse []byte) (msg []byte, surb []byte, err error) {
+	if len(rawResponse) < 10 {
+		err = errors.New("Length of received response is too short")
+		return
+	}
+
+	withReply := rawResponse[1]
+	if withReply > 1 {
+		err = fmt.Errorf("Invald value for withReply received: %#02x", withReply)
+		return
+	}
+
+	if withReply == 1 {
+		surbLen := int(binary.BigEndian.Uint64(rawResponse[2:10]))
+		surb = rawResponse[10 : 10+surbLen]
+		if len(surb) != surbLen {
+			err = errors.New("Length of surb does not match surbLen")
+			return
+		}
+
+		if len(rawResponse) < 18+surbLen {
+			err = errors.New("Length of received response is too short")
+			return
+		}
+
+		msgLen := int(binary.BigEndian.Uint64(rawResponse[10+surbLen : 18+surbLen]))
+		msg = rawResponse[18+surbLen:]
+		if len(msg) != msgLen {
+			err = errors.New("Length of msg does not match msgLen")
+			return
+		}
+	}
+
+	msgLen := int(binary.BigEndian.Uint64(rawResponse[2:10]))
+	msg = rawResponse[10:]
+	if len(msg) != msgLen {
+		err = errors.New("Length of msg does not match msgLen")
+		return
+	}
+
+	return
 }
 
 func New(clientUri string) (*Client, error) {
@@ -86,10 +168,56 @@ func (c *Client) Reply(replySURB []byte, message []byte) error {
 	return err
 }
 
-func (c *Client) Recv() {
-	// c.readMutex.Lock()
-	// messageType, rawResponse, err := c.conn.ReadMessage()
-	// c.readMutex.Unlock()
+func (c *Client) Recv() (*Response, error) {
+	c.readMutex.Lock()
+	messageType, rawResponse, err := c.conn.ReadMessage()
+	c.readMutex.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if messageType != websocket.BinaryMessage {
+		return nil, errors.New("Recieved response is not of type binary")
+	}
+
+	if len(rawResponse) == 0 {
+		return nil, errors.New("Received response is empty")
+	}
+
+	switch responseTag := rawResponse[0]; responseTag {
+	case errorResponseTag:
+		errCode, msg, err := parseErrorResponse(rawResponse)
+		if err != nil {
+			return nil, err
+		}
+		return &Response{
+			Type: "error",
+			Error: &ErrorResponse{
+				Code:    errCode,
+				Message: msg,
+			},
+		}, nil
+	case receivedResponseTag:
+		msg, surb, err := parseReceivedResponse(rawResponse)
+		if err != nil {
+			return nil, err
+		}
+		return &Response{
+			Type: "received",
+			Received: &ReceivedResponse{
+				Message: msg,
+				Surb:    surb,
+			},
+		}, nil
+	case selfAddressResponseTag:
+		return &Response{
+			Type:        "selfAddress",
+			SelfAddress: rawResponse[1:],
+		}, nil
+	default:
+		return nil, fmt.Errorf("Unknown response tag received: %#02x", responseTag)
+	}
 }
 
 func (c *Client) Close() error {
