eb10ba3b4929ad5a8bb62aa3ab47e936bfb52268
Mukundan314 <mukundan314@gmail.com>
2021-11-26 09:54:45 +0530

    improv: setup logging

--- server/main.go
+++ server/main.go
@@ -1,4 +1,44 @@
 package main
 
+import (
+	"flag"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/mukundan314/nym-cloud/server/nymclient"
+)
+
+var (
+	nymClientUri = flag.String("n", "ws://localhost:1977", "uri to a nym-client")
+	logLevel = flag.String("l", "info", "log level to use; available options are debug, info, warn and error")
+)
+
 func main() {
+	flag.Parse()
+
+	switch *logLevel {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.WithField("logLevel", *logLevel).Error("Unknown log level")
+	}
+
+	client, err := nymclient.New(*nymClientUri)
+	if err != nil {
+		log.WithField("nymClient", *nymClientUri).WithError(err).Fatal("Failed to connect to the nym client")
+	}
+	defer client.Close()
+	log.WithField("nymClient", *nymClientUri).Debug("Successfully connected to the nym client")
+
+	for {
+		_, err := client.Recv()
+		if err != nil {
+			log.WithError(err).Error("An error occurred when receiving data from the nym client")
+		}
+	}
 }
